31e9051d2976da273dbbcf468c1545764b90f86c
abeermosaad <abeermosaad69@gmail.com>
2024-02-02 00:58:47 +0200

    [Enhance] 0-validate_utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -4,35 +4,22 @@
 
 def validUTF8(data):
     """Determines if a given data set represents a valid UTF-8 encoding"""
-    binary_list = []
+    num_bytes_to_follow = 0
     for char in data:
-        if char > 247:
-            return False
-        binary_list.append(format(char, '#010b')[-8:])
-    # print(binary_list)
-
-    length = len(binary_list)
-    for idx in range(length):
-        if binary_list[idx][:5] == '11110' and length >= idx + 4:
-            if binary_list[idx + 1][:2] == '10' and \
-                binary_list[idx + 2][:2] == '10' and \
-                    binary_list[idx + 3][:2] == '10':
-                idx += 4
+        binary = format(char, '#010b')[-8:]
+        if num_bytes_to_follow == 0:
+            if binary.startswith('0'):
+                continue
+            elif binary.startswith('110'):
+                num_bytes_to_follow = 1
+            elif binary.startswith('1110'):
+                num_bytes_to_follow = 2
+            elif binary.startswith('11110'):
+                num_bytes_to_follow = 3
             else:
                 return False
-        elif binary_list[idx][:4] == '1110' and length >= idx + 3:
-            if binary_list[idx + 1][:2] == '10' and\
-                  binary_list[idx + 2][:2] == '10':
-                idx += 3
-            else:
-                return False
-        elif binary_list[idx][:3] == '110' and length >= idx + 2:
-            if binary_list[idx + 1][:2] == '10':
-                idx += 2
-            else:
-                return False
-        elif binary_list[idx][:2] == '10' or binary_list[idx][:1] == '0':
-            idx += 1
         else:
-            return False
-    return True
+            if not binary.startswith('10'):
+                return False
+            num_bytes_to_follow -= 1
+    return num_bytes_to_follow == 0
